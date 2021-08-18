2026f041c51557cd403641b5e47ebdedd0569584
oghenemavo <princetunes@gmail.com>
2021-08-18 12:13:29 +0100

    route changes

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $show = 'Hello welcome, ';
+    if (auth()->user()) { 
+        $show .= '<a href="admin/logout">logout</a>';
+    } else {
+        $show .= '<a href="admin">admin here</a>';   
+    }
+    echo $show;
 });
 
 
